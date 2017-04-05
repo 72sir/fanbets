cf6b7f17b2ce1c9bbaedd83221512e55466141da
Alexandr Sirotkin <72gsir@gmail.com>
2017-04-05 21:01:01 +0300

    mobil vers.

--- index/views.py
+++ index/views.py
@@ -6,33 +6,25 @@ from django.shortcuts import render
 
 URL = 'http://betting.gg11.bet/api/sportmatch/GetMostPopular?sportID=2357'
 ALL_GAME = 'http://betting.gg11.bet/api/sportmatch/Get?sportID=2357'
-NAME_GAME = ["Counter-Strike", "Overwatch", "Dota 2", "League of Legends", "World of Tanks"]
-
 
 # Create your views here.
 
 
 def index(request):
-    args = parse_courses(URL)
-    list_game = parse_game(args)
+    html = json_data_parser_html(URL)
     args = {
-        "products": args,
+        "html": html,
         "bar": "/",
-        "NAME_GAME": list_game
     }
-
     return render(request, 'index.html', args)
 
 
 def all_game(request):
-    args = parse_courses(ALL_GAME)
-    list_game = parse_game(args)
+    html = json_data_parser_html(ALL_GAME)
     args = {
-        "products": args,
+        "html": html,
         "bar": "all_game",
-        "NAME_GAME": list_game
     }
-
     return render(request, 'index.html', args)
 
 
@@ -46,59 +38,98 @@ def json_all_game_html(request):
     return HttpResponse(html)
 
 
+def statistica(request):
+    html = "<h1>Статистика еще в разработке.</h1>"
+    args = {
+        "html": html,
+        "bar": "statistica",
+    }
+    return render(request, 'index.html', args)
+
+
 def json_data_parser_html(url):
     # Функция парсит данные json и возвращает таблицу с результатом
-    products = parse_courses(url)
-    NAME_GAME = parse_game(products)
+    all_game_json = parse_courses(url)
+    category_game = parse_game(all_game_json)
+
+    tournament_game = category_game["tournament_game"]
+    category_game = category_game["list_game"]
 
     html = ""
-    for game in NAME_GAME:
-        html += "<h1>" + game + "</h1><table class='table table-striped table-hover'><thead><tr><th> </th><th> </th>" \
+    for category in category_game:
+        html += "<h1>" + category + "</h1><br>"
+        for tournamet in tournament_game:
+            cat_bool = True
+            for product in all_game_json:
+                if product["Category"] == category and product["Tournament"] == tournamet:
+                    if cat_bool:
+                        html += "<p style='font-size: 20px;'>" + tournamet + "</p><table class='table table-striped table-hover'><thead><tr><th> </th><th> </th>" \
                                 "<th> </th><th> </th><th> </th><th> </th></tr></thead><tbody>"
-        for product in products:
-            if product["Category"] == game:
-                html += "<tr style='font-size: 18px'><td><span style='color:black'> " + str(
-                    product["Date"]) + " </span>" \
-                                       "<b>" + str(product["MonthNameShort"]) + "</b><u> " + str(
-                    product["Time"]) + " </u></td><td>" \
-                                       "" + str(product["Tournament"]) + "</td><td>"
+                        cat_bool=False
 
-                if product["HomeTeamLogo"]:
-                    html += "<a href='" + str(
-                        product["HomeTeamLogo"]) + "' target='_blank'><img style='height: 30px;' src='" \
-                                                   "" + str(product["HomeTeamLogo"]) + "' alt='blog post'></a>"
+                    html += "<tr style='font-size: 18px'><td style='width: 125px;'><span style='color:black'> " + str(
+                            product["Date"]) + " </span>" \
+                            "<b>" + str(product["MonthNameShort"]) + "</b> <u>" + str(
+                            product["Time"]) + " </u></td><td style='width: 64px; text-align: center;'>" \
+                            "<a href='" + str(product["StreamUrl"]) + "' target='_blank'> Video </a> </td><td style='text-align: center;'>"
 
-                html += "<a href='" + str(product["StreamUrl"]) + "' target='_blank'><span class='red'>" + str(
-                    product["Title_0"]) + "" \
-                                          "</span><span class='blue'>VS</span><span class='red'>" + str(
-                    product["Title_1"]) + "</span></a>"
+                    if product["HomeTeamLogo"]:
+                        html += "<a href='" + str(
+                            product["HomeTeamLogo"]) + "' target='_blank'><img style='height: 30px;' src='" \
+                                                       "" + str(product["HomeTeamLogo"]) + "' alt='blog post'></a>"
 
-                if product["OriginalAwayTeamLogo"]:
-                    html += "<a href='" + product[
-                        "OriginalAwayTeamLogo"] + "' target='_blank'><img style='height: 30px;' " \
-                                                  "src='" + product["OriginalAwayTeamLogo"] + "' alt='blog post'></a>"
+                    html += "<span class='red'> " + str(
+                        product["Title_0"]) + "</span><span class='blue'> VS </span><span class='red'>" + str(
+                        product["Title_1"]) + "</span> "
 
-                html += "</td><td class='fz_21'>" + str(product["Value_0"]) + "</td><td class='fz_21'>" + str(
-                    product["Value_1"]) + "</td>" \
-                                          "<td><a class='btn btn-primary btn-sm btn-block' href='http://betting.gg11.bet/#/sport/match/" \
-                                          "" + str(product["MatchID"]) + "'>+ " + str(
-                    product["MarketsCount"]) + "</a></td></tr>"
-        html += "</tbody></table>"
+                    if product["OriginalAwayTeamLogo"]:
+                        html += "<a href='" + product[
+                            "OriginalAwayTeamLogo"] + "' target='_blank'><img style='height: 30px;' " \
+                                                      "src='" + product["OriginalAwayTeamLogo"] + "' alt='blog post'></a>"
 
+                    html += "</td><td class='fz_21' style='min-width: 60px; width: 60px;'>" + str(product["Value_0"]) + \
+                            "</td><td class='fz_21' style='min-width: 60px; width: 60px;'>" + str(
+                            product["Value_1"]) + "</td><td style='width: 100px;'><a class='btn btn-primary btn-sm btn-block' " \
+                            "href='http://betting.gg11.bet/#/sport/match/" + str(product["MatchID"]) + "'>+ " + str(
+                            product["MarketsCount"]) + "</a></td></tr>"
+            html += "</tbody></table>"
     return html
 
 
 def parse_game(arr_game):
     list_game = []
+    catergory_game = []
+    tournament_game = []
+
+    for catergory in arr_game:
+        bool_category = True
+        for game in catergory_game:
+            if catergory["Category"] == game:
+                bool_category = False
+        if bool_category:
+            catergory_game.append(catergory["Category"])
+
+    for tournament in arr_game:
+        bool_category = True
+        for game in tournament_game:
+            if tournament["Tournament"] == game:
+                bool_category = False
+        if bool_category:
+            tournament_game.append(tournament["Tournament"])
 
     # search game in all information game arr
-    for game in NAME_GAME:
+    for game in catergory_game:
         bool_game = True
         for elem in arr_game:
             if elem["Category"] == game and bool_game:
                 list_game.append(game)
                 bool_game = False
-    return list_game
+    args = {
+        "list_game": list_game,
+        "tournament_game": tournament_game
+    }
+
+    return args
 
 
 def parse_courses(url):
@@ -116,7 +147,6 @@ def parse_courses(url):
 
     try:
         f_json = json.loads(f_read)
-
         while True:
             try:
                 _str = str(f_json[i]["DateOfMatchLocalized"]["Date"])
